3ff17f47c3b8d2185f1f10c5344a2332caec9353
Dominik Krakowiak <thinex@wp.pl>
2025-02-23 12:54:04 +0100

    chore: update Footer component layout for improved responsiveness and styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <div className="inline-flex justify-between w-full h-min py-2 px-4 items-center text-gray-500 text-sm">
+    <div className="flex flex-col sm:flex-row sm:justify-between w-full h-min py-2 px-4 items-center text-gray-500 text-sm text-center gap-4 bg-transparent absolute bottom-0">
       <h1>
         © 2025 by <Link className="text-gray-400" href="https://github.com/th11n">Dominik Krakowiak</Link>{" "}
         and <Link className="text-gray-400" href="https://github.com/style77">Joachim Hodana</Link>. All
