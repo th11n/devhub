8a0b4f83857e258b7ba6640e51fc9533089e189f
Dominik Krakowiak <thinex@wp.pl>
2025-04-15 16:38:07 +0200

    refactor: remove GET endpoint and refactor POST endpoint

--- client/app/api/posts/route.ts
+++ client/app/api/posts/route.ts
@@ -1,49 +1,7 @@
-import { pool } from "@/lib/db";
+import { ResourceService } from "@/lib/services/resource-service";
 import { NextRequest, NextResponse } from "next/server";
 
-const maxPerPage = 12;
 const REQUIRED_TOKEN = process.env.API_KEY;
-
-export async function GET(request: NextRequest) {
-  const pageParam = request?.nextUrl?.searchParams.get("page");
-  const page = isNaN(Number(pageParam)) ? 0 : Number(pageParam);
-
-  const offset = page * maxPerPage;
-  const category = request?.nextUrl?.searchParams.get("filterBy");
-  const status = request?.nextUrl.searchParams.get("status") || "published";
-  const client = await pool.connect();
-  try {
-    let res;
-    let pageCountRes;
-    if (category) {
-      res = await client.query(
-        `SELECT * FROM posts WHERE status = $4 AND category = $3 ORDER BY id LIMIT $1 OFFSET $2`,
-        [maxPerPage, offset, category, status]
-      );
-      pageCountRes = await client.query(
-        "SELECT count(id) FROM posts WHERE status=$2 AND category = $1",
-        [category, status]
-      );
-    } else {
-      console.log(page, offset, maxPerPage);
-      res = await client.query(
-        `SELECT * FROM posts WHERE status=$3 ORDER BY id LIMIT $1 OFFSET $2`,
-        [maxPerPage, offset, status]
-      );
-      pageCountRes = await client.query(
-        "SELECT count(id) FROM posts WHERE status=$1",
-        [status]
-      );
-    }
-    const data = res.rows;
-    return Response.json({
-      data,
-      pageCount: Math.ceil(pageCountRes.rows[0].count / maxPerPage),
-    });
-  } finally {
-    client.release();
-  }
-}
 
 export async function POST(req: NextRequest) {
   const authHeader = req.headers.get("authorization");
@@ -86,14 +44,19 @@ export async function POST(req: NextRequest) {
   const safeImage = truncate(image, MAX_LEN.image);
   const safeCategory = truncate(category, MAX_LEN.category);
 
-  try {
-    const result = await pool.query(
-      `INSERT INTO posts (title, body, status, url, created_at, category, image)
-       VALUES ($1, $2, $3, $4, NOW(), $5, $6) RETURNING id`,
-      [safeTitle, content, "published", safeUrl, safeCategory, safeImage]
-    );
+  const resourceService = new ResourceService();
 
-    return NextResponse.json({ success: true, id: result.rows[0].id });
+  try {
+    const result = await resourceService.createResource({
+      title: safeTitle!,
+      body: content,
+      url: safeUrl!,
+      category: safeCategory!,
+      status: 'published',
+      image: safeImage || "",
+    });
+
+    return NextResponse.json({ success: true, id: result.id });
   } catch (err) {
     console.error("DB insert error:", err);
     return NextResponse.json(
