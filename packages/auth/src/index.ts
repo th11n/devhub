15194f48dd5cd27f1ee4433f65fb087aa5730345
th1n <thinex@wp.pl>
2026-02-22 16:31:44 +0100

    chore(auth): update packages/auth/src/index.ts

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -5,21 +5,31 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg",
+	database: drizzleAdapter(db, {
+		provider: "pg",
 
-    schema: schema,
-  }),
-  trustedOrigins: [env.CORS_ORIGIN],
-  emailAndPassword: {
-    enabled: true,
-  },
-  advanced: {
-    defaultCookieAttributes: {
-      sameSite: "none",
-      secure: true,
-      httpOnly: true,
-    },
-  },
-  plugins: [],
+		schema: schema,
+	}),
+	trustedOrigins: [env.CORS_ORIGIN],
+	emailAndPassword: {
+		enabled: true,
+	},
+	socialProviders: {
+		github: {
+			clientId: env.GITHUB_CLIENT_ID || "",
+			clientSecret: env.GITHUB_CLIENT_SECRET || "",
+		},
+		google: {
+			clientId: env.GOOGLE_CLIENT_ID || "",
+			clientSecret: env.GOOGLE_CLIENT_SECRET || "",
+		},
+	},
+	advanced: {
+		defaultCookieAttributes: {
+			sameSite: "none",
+			secure: true,
+			httpOnly: true,
+		},
+	},
+	plugins: [],
 });
