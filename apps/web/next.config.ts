214b6bf1008188df6f0590ba75d8a5847fcd10fb
th1n <thinex@wp.pl>
2026-02-22 16:31:34 +0100

    chore(web): update apps/web/next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,17 +2,17 @@ import "@devhub/env/web";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typedRoutes: true,
-  reactCompiler: true,
-  transpilePackages: ["@devhub/ui"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-      },
-    ],
-  },
+	typedRoutes: true,
+	reactCompiler: true,
+	transpilePackages: ["@devhub/ui"],
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "cdn.sanity.io",
+			},
+		],
+	},
 };
 
 export default nextConfig;
