68dea2ad3c114959ff6f94f84c28b29002b8023f
th1n <thinex@wp.pl>
2026-04-17 12:59:52 +0200

    chore: pwa

---
+++ apps/web/src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Devhub',
+        short_name: 'Devhub',
+        description: 'High Quality Curated Resources for Developers',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#171717',
+        theme_color: '#171717',
+        icons: [
+            {
+                src: '/web-app-manifest-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/web-app-manifest-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+        ],
+    }
+}
