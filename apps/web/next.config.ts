7a9bd5902e5627605fc7aae8506d950826bdbe4e
th1n <thinex@wp.pl>
2026-04-17 13:43:17 +0200

    chore: build changes

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,7 @@ import "@devhub/env/web";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	output: "standalone",
 	typedRoutes: true,
 	reactCompiler: true,
 	transpilePackages: ["@devhub/ui"],
